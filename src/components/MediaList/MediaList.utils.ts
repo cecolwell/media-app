cd4cc2ee7c7e62838cd4c0e6068e993a2a9bfa9d
Casidy-Colwell <cassidycolwell94@gmail.com>
2022-08-17 10:18:54 +0200

    added default cases to MediaList.utils and created unit tests

--- src/components/MediaList/MediaList.utils.ts
+++ src/components/MediaList/MediaList.utils.ts
@@ -1,21 +1,29 @@
 export const convertRatingToPercentage = (rating?: number) => {
   if (rating) {
     return `${Math.ceil(rating * 10)}%`;
+  } else {
+    return "N/A";
   }
 };
 
 export const getRatingBadgeColor = (rating?: number) => {
-  if (rating) {
-    if (rating > 8) {
-      return "green";
-    } else if (rating > 6) {
-      return "yellow";
-    } else {
-      return "red";
-    }
+  let badgeColor;
+  switch (true) {
+    case rating && rating > 8:
+      badgeColor = "green";
+      break;
+    case rating && rating > 6:
+      badgeColor = "yellow";
+      break;
+    case rating && rating <= 6:
+      badgeColor = "red";
+      break;
+    default:
+      badgeColor = "#01b4e4";
   }
+  return badgeColor;
 };
 
 export const getReleaseYear = (releaseDate?: string) => {
-  return releaseDate?.split("-")[0];
+  return releaseDate ? releaseDate?.split("-")[0] : "N/A";
 };
