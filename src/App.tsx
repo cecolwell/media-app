2977d2a0e5d6ec7abcde4fd1ddf0b937d777f9d6
Casidy-Colwell <cassidycolwell94@gmail.com>
2022-08-17 09:08:01 +0200

    replaced hard-coded values with const and removed unsued className

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,33 @@ import { Outlet } from "react-router-dom";
 import { Layout } from "antd";
 
 import { MainMenu } from "./components/MainMenu/MainMenu";
+import {
+  THE_MOVIE_DB_LOGO,
+  THE_MOVE_DB_URL,
+  THE_MOVIE_DB,
+  FOOTER_TEXT,
+  HEADER_TEXT,
+} from "./constants";
 import "./App.css";
 
 const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Layout>
         <Header className="header">
-          Popular Now <MainMenu />
+          {HEADER_TEXT}
+          <MainMenu />
         </Header>
         <Content className="content">
           <Outlet />
         </Content>
         <Footer className="footer">
-          <img
-            src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
-            alt="THE MOVIE DB"
-            width="100"
-          />
+          <img src={THE_MOVIE_DB_LOGO} alt={THE_MOVIE_DB} width="100" />
           <span className="footerText">
-            {" "}
-            All data and images have been pulled from
-            <a href="https://www.themoviedb.org/">The Movie DB</a>
+            {FOOTER_TEXT}
+            <a href={THE_MOVE_DB_URL}>{THE_MOVIE_DB}</a>
           </span>
         </Footer>
       </Layout>
